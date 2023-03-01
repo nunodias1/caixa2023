8611f1722f8b91689e647cb0a3f9581afbff0f00
Geral <Geral@OMEN-NUNO>
2023-03-01 14:42:01 +0000

    Parameterizar caixa

--- src/pt/nunodias/caixa/Main.java
+++ src/pt/nunodias/caixa/Main.java
@@ -4,17 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		printTopOrBottom();
+		printTopOrBottom(4);
 		System.out.println("*    *");
 		System.out.println("*    *");
 		System.out.println("*    *");
-		System.out.println("*    *");
-		printTopOrBottom();
+		printTopOrBottom(4);
 		
 	}
 
-	public static void printTopOrBottom() {
-		System.out.println("*******");
+	public static void printTopOrBottom(int size) {
+		for (int i = 0; i < size; i++) {
+			System.out.print("*");
+		}
+			System.out.println();
 	}
 	
 }
