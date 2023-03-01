5901e575fb632ec316c1d84203bcb808e8af6576
Geral <Geral@OMEN-NUNO>
2023-03-01 13:56:48 +0000

    First Commit

---
+++ src/pt/nunodias/caixa/Main.java
@@ -0,0 +1,16 @@
+package pt.nunodias.caixa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("******");
+		System.out.println("*    *");
+		System.out.println("*    *");
+		System.out.println("*    *");
+		System.out.println("*    *");
+		System.out.println("******");
+		
+	}
+
+}
