69432772435ed9ea46e8c92dad5a64495449f05f
Geral <Geral@OMEN-NUNO>
2023-03-01 15:18:23 +0000

    Criar Caixa

--- src/pt/nunodias/caixa/Main.java
+++ src/pt/nunodias/caixa/Main.java
@@ -4,12 +4,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		printTopOrBottom(6);
-		printLeftAndRight(6, 4);
-		printTopOrBottom(6);
-		
+
+		printBox(4,4);
+		printBox(8,4);
+		printBox(5,5);
 	}
 
+	public static void printBox(int width, int height) {
+		if (width < 4) {
+			width = 4;
+		}
+		if (height < 4) {
+			height = 4;
+		}
+		printTopOrBottom(width);
+		printLeftAndRight(width, height-2);
+		printTopOrBottom(width);
+	}
+	
 	public static void printTopOrBottom(int size) {
 		for (int i = 0; i < size; i++) {
 			System.out.print("*");
